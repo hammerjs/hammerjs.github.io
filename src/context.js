f92eb2ec289996bd80e44d25ab6f5ffb735d7e91
J. Tangelder <j.tangelder@gmail.com>
2014-07-10 15:50:01 +0200

    highlight.js

--- src/context.js
+++ src/context.js
@@ -1,12 +1,22 @@
+var marked = require('marked');
 var fs = require('fs');
 var gzipSize = require('gzip-size');
 var prettyBytes = require('pretty-bytes');
+var highlight = require('highlight.js')
+
+
+marked.setOptions({
+  highlight: function (code, lang) {
+      console.log(code, lang);
+    return highlight.highlightAuto(code, [lang]).value;
+  }
+});
 
 
 module.exports = {
     version: (function() {
         return JSON.parse(
-            fs.readFileSync('./hammerjs/package.json', {encoding:'utf8'})
+            fs.readFileSync('./node_modules/hammerjs/package.json', {encoding:'utf8'})
         ).version;
     })(),
 
