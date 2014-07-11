f0b0c1330185ddb1166045c700fb3f1a057b1675
Jorik Tangelder <j.tangelder@gmail.com>
2014-07-11 16:03:13 +0200

    analytics code

--- src/assets/js/externals.js
+++ src/assets/js/externals.js
@@ -1,16 +1,10 @@
-if(location.href.indexOf(".github.io") > -1) {
-    var _gaq = _gaq || [];
-    _gaq.push(['_setAccount', 'UA-30289566-1']);
-    _gaq.push(['_trackPageview']);
-    (function () {
-        var ga = document.createElement('script');
-        ga.type = 'text/javascript';
-        ga.async = true;
-        ga.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'stats.g.doubleclick.net/dc.js';
-        var s = document.getElementsByTagName('script')[0];
-        s.parentNode.insertBefore(ga, s);
-    })();
-}
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-30289566-1', 'auto');
+ga('send', 'pageview');
 
 !function (d, s, id) {
     var js, fjs = d.getElementsByTagName(s)[0];
